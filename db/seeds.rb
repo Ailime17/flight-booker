e1672400bb5ed315cce9e1a90521a1dedf1b9122
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-06 12:05:11 +0200

    add Airport model and seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: '"St'ar Wars" }, { name: '"Lo'rd of the Rings" }])
+#   Character.create(name: '"Lu'ke", movie: 'mov'ies.first)
+
+Airport.create(code: 'TNR')
+Airport.create(code: 'NYC')
+Airport.create(code: 'ATH')
+Airport.create(code: 'ATL')
+Airport.create(code: 'WAW')
+Airport.create(code: 'WIC')
+Airport.create(code: 'LCY')
+Airport.create(code: 'LWO')
