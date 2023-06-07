144702ec673aede59e29f5988c4be81ea48819dd
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-07 21:46:46 +0200

    create flights#index view; form with dropdown menus to search for a flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @departure_airport_options = Flight.select(:departure_airport_id).distinct.map { |f| [f.departure_airport.code, f.departure_airport.code] }
+    @arrival_airport_options = Flight.select(:arrival_airport_id).distinct.map { |f| [f.arrival_airport.code, f.arrival_airport.code] }
+    @date_options = Flight.all.map { |f| [f.date.strftime("%k:%M, %d(%A)-%m-%Y"), f.date] }
   end
 end
