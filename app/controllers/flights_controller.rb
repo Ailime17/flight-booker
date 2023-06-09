2a49c6009391bb7b1dc8f86ba6a40b2861ec0f95
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-09 12:13:59 +0200

    find flights that match the search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,19 @@ class FlightsController < ApplicationController
   def index
     @departure_airport_options = Flight.select(:departure_airport_id).distinct.map { |f| [f.departure_airport.code, f.departure_airport.code] }
     @arrival_airport_options = Flight.select(:arrival_airport_id).distinct.map { |f| [f.arrival_airport.code, f.arrival_airport.code] }
-    @date_options = Flight.all.map { |f| [f.date.strftime("%k:%M, %d(%A)-%m-%Y"), f.date] }
+    @date_options = Flight.all.map { |f| [f.date.strftime("%d(%A)-%m-%Y"), f.date.strftime("%d-%m-%Y")] }
+    dates_array = []
+    @date_options.each do |d|
+      dates_array << d unless dates_array.include?(d)
+    end
+    @date_options = dates_array
+    if params.key?('flight_data')
+      flights_array = []
+      @matching_flights = Flight.where(departure_airport: Airport.find_by(code: params['flight_data']['departure_airport'])).where(arrival_airport: Airport.find_by(code: params['flight_data']['arrival_airport']))
+      @matching_flights.each do |f|
+        flights_array << f if f.date.strftime("%d-%m-%Y") == params['flight_data']['date']
+      end
+      @matching_flights = flights_array
+    end
   end
 end
