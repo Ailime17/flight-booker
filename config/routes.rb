25be3309a48d1237674d2db07c139e9d9963c704
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-10 15:14:44 +0200

    add routes for bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'flights/index'
+  resources :bookings, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "flights#index"
