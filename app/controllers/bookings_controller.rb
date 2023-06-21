79feadcf10c9b16dc6545bdb2b241b82f1972d31
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-21 21:19:09 +0200

    add #new action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
 class BookingsController < ApplicationController
   def new
-    
+    @flight_id = params["chosen_flight_id"]
+    @chosen_flight = Flight.find(@flight_id)
+    @num_of_passengers = params["passengers_num"].to_i
+
+    @booking = Booking.new(flight_id: @flight_id)
+    @num_of_passengers.times do
+      @booking.passengers.build
+    end
   end
 
   def create
