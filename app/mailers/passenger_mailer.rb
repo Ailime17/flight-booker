fbb8a26ce9c6db49eb9af6a7a6fe447027a7698d
Emilia <emilia.skrzypczak.es@gmail.com>
2023-07-18 09:47:23 +0200

    create confirmation_email action

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: email_address_with_name('example@example.com', 'HeavenlyAirlines')
+
+  def confirmation_email
+    @booking = params[:booking]
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: "Booking ##{@booking.id} confirmation")
+  end
 end
